32264af947591e0da2b983e9153dbea64b9d4595
readycyr <readycyr@hotmail.fr>
2015-10-16 15:34:42 +0200

    uniformatisation of box configuration, add require graphic module

--- ui.R
+++ ui.R
@@ -1,13 +1,7 @@
-makeCSVFileInput <- function(varName, nCol) {
-  column(nCol,
-         column(4, checkboxInput('header', 'Header', TRUE) ),
-         column(4, radioButtons( 'sep', 'Separator', c(Comma = ',', Semicolon = ';',Tab = '\t'), ',', inline = FALSE) ),
-         column(4, radioButtons( 'quote', 'Quote', c(None = '', 'Double Quote' = '"', 'Single Quote' = "'"), selected = '', inline = FALSE) ),
-         fileInput(varName, label = HTML("<p>Choose <span style='color:red;'>file</span> to upload</p>"), 
-          accept = c( 'text/csv', 'text/comma-separated-values', 'text/tab-separated-values', 'text/plain', '.csv', '.tsv' ))
-  )
-}
-
+##########################################
+##                  UI                  ##
+##########################################
+source("chooser.R")
 # A dashboard has three parts: a header, a sidebar, and a body.
 shinydashboard::dashboardPage(
   ## The header content
@@ -21,7 +15,8 @@ shinydashboard::dashboardPage(
     shinydashboard::sidebarMenu(
       shinydashboard::menuItem( "Site performance", tabName = "siteperf", icon = icon("dashboard") ),
       shinydashboard::menuItem( "Dimension preferences", tabName = "dimpref", icon = icon("th") ),
-      shinydashboard::menuItem( "User guide", tabName = "userg", icon = icon("file") )
+      shinydashboard::menuItem( "User guide", tabName = "userg", icon = icon("file") ),
+      sliderInput("mood", "What is your moood", min = 1,max = 100, value = 50, step = 1, ticks = TRUE)
     )
   ),
   ## The body content
@@ -32,32 +27,34 @@ shinydashboard::dashboardPage(
     ),
     shinydashboard::tabItems(
       shinydashboard::tabItem( tabName = "siteperf",
-                               shinydashboard::box(width = 12, 
+                               shinydashboard::box( title = NULL,
+                                                   collapsible = TRUE, collapsed = FALSE, width = 12, 
+                                                   status = NULL, background = "black", solidHeader = TRUE,
                                                    widgetHierarchy::widgetHierarchyOutput("ahpTree", height = '600px') 
-                                ),
+                              ),
                                fluidRow(
                                  shinydashboard::box( title = "AHP TREE file structure",
                                                       collapsible = TRUE, collapsed = FALSE, width = 6,
-                                                      status = "success", solidHeader = TRUE, # background = 'green',
+                                                      status = "info", background = NULL, solidHeader = TRUE,
                                                       rhandsontable::rHandsontableOutput("exampleSheetTree", height = '500px')
                                  ),
                                  shinydashboard::box( title = "AHP TREE initialisation",
                                                       collapsible = TRUE, collapsed = FALSE, width = 6,
-                                                      status = "primary", solidHeader = TRUE, # background = 'green',
+                                                      status = "primary", background = NULL, solidHeader = TRUE,
                                                       HTML("<p>You should <span style='color:red;'>upload</span> a file containing the required data to build the AHP tree.</p>
                                                       <p>Please choose required option for your csv file</p>"),
                                                       makeCSVFileInput('fileTree', 12)
-                                                      #, footer = "here is the footer "
+                                                      #, footer = "There is a footer now"
                                  )
                               ),
-                              uiOutput("treeDataBox")
+                              uiOutput("ahpConfigurationBox"),
+                              uiOutput("uiValueBasedWhatever") 
                                #                                
                                #                                fluidRow(
                                #                                  shinydashboard::box(
                                #                                    checkboxInput("workingTree", "Check the box to start AHP process", value =  FALSE),
-                               #                                    sliderInput("mood", "What is your moood", min = 1,max = 100, value = 50, step = 1, ticks = TRUE)
+                               #                                    
                                #                                  ),
-                               #                                  uiOutput("listMenuBoxMatrix")
       ),
       shinydashboard::tabItem( tabName = "dimpref",
                                fluidRow(
