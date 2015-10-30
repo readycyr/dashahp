27176dada65d7f43b64a72b45431520e96f3db92
ccyril <cyril@jessie64DEV>
2015-10-30 21:30:45 +0100

    Topsis is working, we did it :)

--- server.R
+++ server.R
@@ -7,12 +7,28 @@ makeCriteriaListMenu <- function(id, alabel, crit1, crit2) {
 makeSaatyListMenu <- function(id, alabel) {
   sliderInput( id, alabel, min = 1,max = 9, value = 5, step = 2, ticks = FALSE, post = "x more important")
 }
+sylEigenValue <- function(matx) {
+  dd <- dim(matx)
+  fmat <- c()
+  sum_call <- colSums(matx)
+  for( j in seq_len(dd[2]) ) {
+    if(sum_call[j] == 0) {
+      fmat <- c(fmat, rep(0, each=dd[1]) )
+    } else {
+      fmat <- c(fmat, matx[,j]/sum_call[j])
+    }
+  }
+  fmat<- matrix(fmat, byrow = FALSE, ncol = dd[2])
+  normalized_fmat <- c()
+  for( i in seq_len(dd[1])) {
+    normalized_fmat <- c(normalized_fmat, sum(fmat[i,])/dd[1])
+  }
+  normalized_fmat  
+}
 myEigenValue <- function(matx) {
-
   sqOne <- matx %*% matx
   sqTwo <- sqOne %*% sqOne
   sqMatx <- sqTwo %*% sqTwo
-  #
   dimRow <- nrow(matx)
   vMat <- vector(mode = "numeric", length = dimRow)
   for (i in 1:dimRow ) {
@@ -45,12 +61,29 @@ matrix_basedMeasurement <- function(col_A_Cj) {
   #    for ( k in 1:126) { print( all.equal(as.numeric(tt[k,]), mThree[k,]) ) }
 }
 
-topsis_ranking <- function(col_A_Cj) {
-  nis <- min(col_A_Cj)
-  pis <- max(col_A_Cj)
-  ####
+topsis_ranking <- function(matx) {
+  colMax <- apply(matx,2,max)
+  colMin <- apply(matx,2,min)
+  dd <- dim(matx)
+  d_al <- c()
+  for( i in seq_len(dd[1])) {
+    sum_res_p <- 0
+    sum_res_n <- 0
+    for( j in seq_len(dd[2])) {
+      sum_res_p <- sum_res_p + ( matx[i,j] - colMax[j] )^2
+      sum_res_n <- sum_res_n + ( matx[i,j] - colMin[j] )^2
+    }
+    d_al <- c(d_al, sqrt(sum_res_n), sqrt(sum_res_p))
+  }
+  md_al <- matrix(d_al, byrow = TRUE, ncol = 2)
+  r_al <- c()
+  for( i in seq_len(dd[1])) {
+    r_al_i <- md_al[i,1]/(md_al[i,2]+md_al[i,1])
+    r_al <- c(r_al, r_al_i)
+  }
+  r_al
 }
-#colMax <- function(data) sapply(data, max, na.rm = TRUE)
+
 #colMin <- function(data) sapply(data, min, na.rm = TRUE)
 ##########################################
 ##                SERVER                ##
@@ -63,6 +96,20 @@ function(input, output) {
   lastUploadedFile <- c()
   valuesTree <- list()
   eigenFrame <- NULL
+  ## Function
+  weights_aggregation <- function(parent, wg, level) {
+    if(level == 2) {
+      valuesTree[[paste0("weight_aggr_",parent)]] <<- wg*valuesTree[[paste0("weight_level_",parent)]]
+    } else {
+      k <- 1
+      lv <- level-1      
+      for(n in na.omit( reactValues$treeData[reactValues$treeData$parent %in% parent, c("name")] )) {
+        nwg <- valuesTree[[paste0("weight_level_",parent)]]
+        weights_aggregation(n, wg*nwg[k], lv)
+        k <- k+1
+      }
+    }
+  }
   # Data to provide end-user an example
   reactValues$treeData <- data.frame(
     name = c( 'Root', 
@@ -301,14 +348,19 @@ function(input, output) {
                     if ( input$typeOfMeasurement == "Based preferences" ) {
                         vMat <- myEigenValue(intMatx)
                         vMatSum <- sum(vMat)
-#valuesTree[[paste0("weight_level_",input$treeLevelChoice)]] <<- (vMat/vMatSum)
                         vectorRepresentation <- c(vectorRepresentation, " $$ \\\\Longrightarrow ")
                         vectorRepresentation <- c(vectorRepresentation, "  \\\\begin{bmatrix} ")
                         vectorRepresentation <- c(vectorRepresentation, paste0( specify_digits((vMat/vMatSum), 3)," \\\\\\\\ "))
                         vectorRepresentation <- c(vectorRepresentation, paste0(" \\\\hline", sum(vMat/vMatSum)," \\\\\\\\ ")) 
                       if( dim(intMatx)[1] > 2 ) {
                         ahppmr <- pmr::ahp(dset = intMatx, sim_size = 500)
-#valuesTree[[paste0("weight_level_",input$treeLevelChoice)]] <<- ahppmr$weighting
+
+## For level 4 only one matrices is created
+if ( input$treeLevelChoice == 2) {
+  valuesTree[[paste0("weight_level_","root")]] <<- ahppmr$weighting
+} else {
+  valuesTree[[paste0("weight_level_",input$subsetTreeChoice)]] <<- ahppmr$weighting
+}
                         vectorRepresentation <- c(vectorRepresentation, " \\\\end{bmatrix} or \\\\begin{bmatrix} ")
                         vectorRepresentation <- c(vectorRepresentation, paste0( specify_digits((ahppmr$weighting), 3)," \\\\\\\\ "))
                         vectorRepresentation <- c(vectorRepresentation, paste0(" \\\\hline", sum(ahppmr$weighting)," \\\\\\\\ ")) 
@@ -318,6 +370,14 @@ function(input, output) {
                                 "\\\\text{Koczkodaj\'s inconsistency} = ", specify_digits((ahppmr$Koczkodaj), 3),
                                 " \\\\end{array}"))
                       } else {
+
+## For level 4 only one matrices is created
+if ( input$treeLevelChoice == 2) {
+  valuesTree[[paste0("weight_level_","root")]] <<- (vMat/vMatSum)
+} else {
+  valuesTree[[paste0("weight_level_",input$subsetTreeChoice)]] <<- (vMat/vMatSum)
+}
+
                         vectorRepresentation <- c(vectorRepresentation, " \\\\end{bmatrix} \\\\\\\\ $$ ")
                       }
                     } else if ( input$typeOfMeasurement == "Based measurements" ) {
@@ -326,33 +386,54 @@ function(input, output) {
                       if ( input$treeLevelChoice > 2) {
                         if ( length(input$subsetTreeChoice) != 0 && input$subsetTreeChoice != 0) {
                                 for(nm in as.character(input$subsetTreeChoice)) {
-                                  if(dim(intMatx[[paste0(nm)]])[1] > 2) {
-                                    res <- pmr::ahp(dset = intMatx[[paste0(nm)]] , sim_size = 500)
-                                    eigenFrame <<- eval(parse(text= paste0("cbind(eigenFrame,",nm," = ",res$weighting,")") ))
+                                  res <- NULL
+                                  if( nrow(intMatx[[paste0(nm)]])[1] > 2 ) {
+                                    # res <- pmr::ahp(dset = intMatx[[paste0(nm)]] , sim_size = 500)
+                                    # eigenFrame <<- eval(parse(text= paste0("cbind(eigenFrame,",nm," = ",res$weighting,")") ))
+                                     res <- sylEigenValue( intMatx[[paste0(nm)]] )
+                                     eigenFrame <<- eval(parse(text= paste0("cbind(eigenFrame,",nm," = ",res,")") ))
+valuesTree[[paste0("weight_level_",nm)]] <<- res #$weighting
                                   } else {
-                                    res <- myEigenValue(intMatx[[paste0(nm)]])
-                                    eigenFrame <<- eval(parse(text= paste0("cbind(eigenFrame,",nm," = ",(res/sum(res)),")") ))
-                                  }
-#valuesTree[[paste0("weight_level_",nm)]] <<- res$weighting
+                                     res <- myEigenValue(intMatx[[paste0(nm)]])
+                                     eigenFrame <<- eval(parse(text= paste0("cbind(eigenFrame,",nm," = ",(res/sum(res)),")") ))
+valuesTree[[paste0("weight_level_",nm)]] <<- (res/sum(res))
+                                   }
                                 }
                         } else {
                             return(NULL)
                         }                                  
                       } else if ( input$treeLevelChoice == 2) {
-                            res <- pmr::ahp(dset = intMatx[["root"]], sim_size = 500)
-                            eigenFrame <- cbind(eigenFrame, root = res$weighting)
-#valuesTree[[paste0("weight_level_","root")]] <<- res$weighting
+                            # res <- pmr::ahp(dset = intMatx[["root"]], sim_size = 500)
+                            # eigenFrame <- cbind(eigenFrame, root = res$weighting)
+                            res <- sylEigenValue(intMatx[["root"]])
+                            eigenFrame <- cbind(eigenFrame, root = res)
+valuesTree[[paste0("weight_level_", "root")]] <<- res #$weighting
                       } else {
                           return(NULL)
                       }
-
-print(eigenFrame)
                     }
-#ln <- na.omit( reactValues$treeData[reactValues$treeData$parent %in% input$subsetTreeChoice, c("name")] )
-#for(nm in ln) {
-#  reactValues$treeData[ reactValues$treeData$name == ln, c("score")] <- $weighting[]
-#}                         
-                         '), file = matrixScript, append = TRUE)
+
+## To update TREE
+nodeName <- NULL
+nodeValues <- NULL
+if ( input$treeLevelChoice <4) {
+   if ( input$treeLevelChoice > 2) {
+     nodeValues <- valuesTree[[paste0("weight_level_",input$subsetTreeChoice)]]
+     nodeName <- as.character(na.omit( reactValues$treeData[reactValues$treeData$parent %in% input$subsetTreeChoice, c("name")] ))
+   } else {
+     nodeValues <- valuesTree[[paste0("weight_level_","root")]]
+     nodeName <- as.character( reactValues$treeData[reactValues$treeData$depth == 2, c("name")] )
+   }
+}
+print("==============================")
+print(nodeName)
+print(nodeValues)
+print("==============================")
+#   for(index in seq_len(length(nodeName))) {
+#     #reactValues$treeData[ reactValues$treeData$name == as.character(nodeName[index]), c("score")] <- as.numeric(nodeValues[index])  
+#   }
+
+  '), file = matrixScript, append = TRUE)
         }
         ## Script use
         if (input$visibleMTX && (length(input$treeLevelChoice) != 0)) {
@@ -443,21 +524,54 @@ print(eigenFrame)
   #####
   output$plothist <- renderPlot({
     if( !is.null(eigenFrame) )  {
-      #valuesTree[["weight_frame"]] <- data.frame()
-      #valuesTree[[paste0("weight_level",input$treeLevelChoice)]]
-      wg <- valuesTree[[paste0("weight_level",3)]]
-      
-      eigenMatrix <- as.matrix( subset( eigenFrame, select = -c(Au_vu_de)))
-      altRanked <- topsis::topsis(decision = eigenMatrix, 
-                                  weights = c(wg[1]),  
-                                  impacts = rep("+",ncol(eigenMatrix)))
-      print(altRanked$score)
-      print(altRanked$rank)
-      criteria <- valuesTree[[paste0("file_level",input$treeLevelChoice)]]
-      
-      barplot(altRanked$score, main = "Site Distribution", horiz = FALSE, 
-               names.arg = as.character(criteria[, c("Au_vu_de")]), cex.names = 1, axis.lty = 1, las = 2)
+      # weight aggregation
+      if ( input$treeLevelChoice == 4) {
+        numberLevel <- names( table(reactValues$treeData$depth) )
+# Work just for this tree
+#       for(lv in 2:(length(numberLevel)-2) ) {
+#         for(p in  names(table( reactValues$treeData[reactValues$treeData$depth == lv, c("parent")] )) ) {
+#           pwg <- valuesTree[[paste0("weight_level_","root")]]
+#           index <- 1
+#           for(n in na.omit( reactValues$treeData[reactValues$treeData$parent %in% p, c("name")] )) {
+#             nwg <- valuesTree[[paste0("weight_level_",n)]]
+#             k <- 1
+#             for( subn in na.omit( reactValues$treeData[reactValues$treeData$parent %in% n, c("name")] ) ) {
+#               valuesTree[[paste0("weight_aggr_",subn)]] <- pwg[index]*nwg[k]*valuesTree[[paste0("weight_level_",subn)]]
+#               k <- k+1
+#             }
+#             index <- index+1
+#           }
+#         }
+#       }
+        # Weight aggragation
+        pwg <- valuesTree[[paste0("weight_level_","root")]]
+        root <- as.character( reactValues$treeData[reactValues$treeData$depth == 1, c("name")] )
+        index <- 1
+        for(node in na.omit( reactValues$treeData[reactValues$treeData$parent %in% root, c("name")] )) {
+          weights_aggregation(node, pwg[index], length(numberLevel)-1)
+          index <- index+1
+        }
+        ultim <- c()
+        for(nm in as.character(input$subsetTreeChoice)) {
+          ultim <- c(ultim, valuesTree[[paste0("weight_aggr_",nm)]] )
+        }
+        theMat <- matrix(ultim, byrow = FALSE, ncol = length(input$subsetTreeChoice))
+        #
+        topMat <- topsis_ranking(theMat)
+        indTopMat <- cbind( 1:length(topMat), topMat )
+        indTopMat <- indTopMat[ order(indTopMat[,2]),]
+        print(indTopMat)
+#         altRanked <- topsis::topsis(decision = theMat, 
+#                                     weights = rep(1, each = dim(theMat)[2]),  
+#                                     impacts = rep("+", each = dim(theMat)[2]))        
+#         print(altRanked$score)
+#         print(altRanked$rank)
+        barplot(topMat, main = "Site Distribution", horiz = FALSE, 
+                       names.arg = as.character(eigenFrame[, c("Au_vu_de")]), cex.names = 1, axis.lty = 1, las = 2)
+      }
       #lines(thesiteX, c(0, thesiteY), col = "red",lwd = 5)      
     }
   })
+  
+  
 }
